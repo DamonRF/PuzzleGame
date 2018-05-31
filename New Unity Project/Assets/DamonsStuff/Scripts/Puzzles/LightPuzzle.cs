92f9b5b15027e7b01d5fef5a7de69da3ddcbfb9d
Damon Fuller <18.damon.fuller@ksd.org>
2018-05-31 12:55:28 -0700

    Light puzzle. DRF

---
+++ New Unity Project/Assets/DamonsStuff/Scripts/Puzzles/LightPuzzle.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightPuzzle : MonoBehaviour {
+
+    public int form = 1;
+    private int correctForm = 0;
+    public bool correctPlace = false;
+    public GameObject manager;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void RestartPuzzle()
+    {
+        form = 1;
+        GetComponent<SpriteRenderer>().color = Color.white;
+        correctForm = Random.Range(1, 5);
+        if (form == correctForm)
+        {
+            correctPlace = true;
+        }
+        else
+        {
+            correctPlace = false;
+        }
+    }
+
+    public void ChangePiece()
+    {
+        form++;
+        if (form >= 4)
+        {
+            form = 1;
+        }
+
+        if (form == 1)
+        {
+            GetComponent<SpriteRenderer>().color = Color.white;
+        } else if (form == 2)
+        {
+            GetComponent<SpriteRenderer>().color = Color.green;
+        } else if (form == 3)
+        {
+            GetComponent<SpriteRenderer>().color = Color.cyan;
+        } else if (form == 4)
+        {
+            GetComponent<SpriteRenderer>().color = new Color(143, 0, 254);
+        }
+
+        if (form == correctForm)
+        {
+            correctPlace = true;
+        } else
+        {
+            correctPlace = false;
+        }
+    }
+}
---
+++ New Unity Project/Assets/DamonsStuff/Scripts/Puzzles/LightPuzzleManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightPuzzleManager : MonoBehaviour {
+
+    public GameObject[] pieces;
+    public bool[] trueOrFalse;
+    public StringArray[] conversation;
+	// Use this for initialization
+	void Start () {
+        PlayerPrefs.SetInt("Chances", 10);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void UpdateTrueOrFalse(bool rightForm, GameObject piece)
+    {
+        int place = 0;
+        for (int i = 0; i < pieces.Length; i++)
+        {
+            if (pieces[i] == piece)
+            {
+                place = i;
+                break;
+            }
+        }
+        if (rightForm)
+        {
+            trueOrFalse[place] = true;
+        }
+        else
+        {
+            trueOrFalse[place] = false;
+        }
+        
+    }
+
+    public void CheckPieces()
+    {
+        int correct = 0;
+        for (int i = 0; i < trueOrFalse.Length; i++)
+        {
+            if (trueOrFalse[i])
+            {
+                correct++;
+            }
+        }
+
+        conversation[1].conversation[1] = correct + " are correct.";
+        if (correct >= pieces.Length){
+            
+        }
+    }
+}
